cba206777b2d3e2d9462752909481b01bedd15c7
shreyash3040 <85779474+shreyash3040@users.noreply.github.com>
2022-12-06 10:47:53 +0530

    Add files via upload

---
+++ django/project/home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('this is homepage')
+    return render(request,'index.html')
+
+def about(request):
+    # return HttpResponse('this is about page')
+    return render(request,'about.html')
+
+def services(request):
+    # return HttpResponse('this is services page')
+    return render(request,'services.html')
+
+def contacts(request):
+    # return HttpResponse('this is contact page')
+    return render(request,'contacts.html')
